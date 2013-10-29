86612989d7d1611e273b38a279cff80c5e8485ed
Emilia Kirschenbaum <ekubokirs@gmail.com>
2013-10-29 09:19:31 -0700

    Initial Commit

---
+++ BlogReaderS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BlogReaderS
+//
+//  Created by Emilia Kirschenbaum on 10/29/13.
+//  Copyright (c) 2013 BedTimeTaco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
